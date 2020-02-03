15c27593db14cc49658eb7ab1bb71c30a039774f
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2020-02-03 09:35:55 +0100

    versión comentada

--- geometria/src/Triangulo_LGC.java
+++ geometria/src/Triangulo_LGC.java
@@ -1,21 +1,34 @@
-
+/**
+ * 
+ * @author Luis García Clavel
+ * @version 1.2
+ *
+ */
 public class Triangulo_LGC extends FiguraGeometrica_LGC {
 	private double lado1;
 	private double lado2;
 	private double lado3;
 	
+	/**
+	 * @param lado indica los lados del triángulo
+	 */
 	public Triangulo_LGC(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
 		this.lado1 = lado1;
 		this.lado2 = lado2;
 		this.lado3 = lado3;
 	}
-
+/**
+ * @return devuelve los lados del triángulo
+ */
 	@Override
 	public double perimetro() {
 		return lado1 + lado2 + lado3;
 	}
-
+	/**
+	 * @param semiPerimetro indica el valor del semiperímetro
+	 * @return devuelve el valor del cuadrado de la multiplicación de cada semiperímetro restándole su lado
+	 */
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
