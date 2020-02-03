e163c8dd7334b307b874b9b0ebb279ea481062a9
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2020-02-03 09:42:37 +0100

    siempre positivo

--- geometria/src/Circulo_LGC.java
+++ geometria/src/Circulo_LGC.java
@@ -7,12 +7,14 @@ public class Circulo_LGC extends FiguraGeometrica_LGC {
 	public Circulo_LGC(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
+		if (radio<0) {
+			radio = radio * -1;
+		}
 	}
-
+	
 	@Override
 	public double area() {
 		return radio * radio * PI;
-
 	}
 	
 	@Override
